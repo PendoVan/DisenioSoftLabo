082850ac83fb5f348ce749e7a24c7fed69a4be65
XJosxx <josx130@gmail.com>
2026-09-17 08:14:03 -0500

    Laboratorio 1 completo

---
+++ Solution1/MiBotica.SolPedido.Entidades/Base/BaseLN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Reflection;
+
+namespace MiBotica.SolPedido.Entidades.Base
+{
+    public class BaseLN
+    {
+        protected log4net.ILog Log
+        {
+            get
+            {
+                return
+                log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().GetType());
+            }
+        }
+    }
+}
---
+++ Solution1/MiBotica.SolPedido.LogicaNegocio/Core/UsuarioLN.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiBotica.SolPedido.AccesoDatos.Core;
+using MiBotica.SolPedido.Entidades.Core;
+using MiBotica.SolPedido.Entidades.Base;
+
+namespace MiBotica.SolPedido.LogicaNegocio.Core
+{
+    public class UsuarioLN : BaseLN
+    {
+
+        public List<Usuario> ListaUsuarios()
+        {
+            try
+            {
+                return new UsuarioDA().ListaUsuarios();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
